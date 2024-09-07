bf93772a7d39b36336529549a403bab2930181e3
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2024-09-07 15:18:30 -0300

    1º commit neste repositório

---
+++ resources/views/index.blade.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Curriculo Pessoal</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!---->
+
+    <link rel="icon" href="briefcase.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body class="w3-light-grey">
+
+    <!-- Page Container -->
+    <div class="w3-content w3-margin-top" style="max-width: 1400px;">
+
+      <!-- The Grid -->
+      <div class="w3-row-padding">
+
+        <!-- Left Column -->
+        <div class="w3-third">
+
+          <div class="w3-white w3-text-grey w3-card-4 w3-round">
+            
+            <div class="w3-display-container">
+              <img src="images/avatar_hat.jpg" class="w3-round w3-image" alt="Avatar">
+
+              <div class="w3-display-bottomleft w3-container w3-brown w3-text-black w3-round" id="w3-container-texto-sobre-imagem">
+                <h2>Matheus Pereira</h2>
+              </div>
+            </div>
+
+            <div class="w3-container">
+              
+              <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-brown"></i>Web - Software Developer</p>
+              <p><i class="fa fa-home      fa-fw w3-margin-right w3-large w3-text-brown"></i>Cristalina, Goiás</p>
+              <p><i class="fa fa-envelope  fa-fw w3-margin-right w3-large w3-text-brown"></i>sap.matheusoliveira@gmail.com</p>
+              <p><i class="fa fa-phone     fa-fw w3-margin-right w3-large w3-text-brown"></i>(98) 99196-8439</p>
+              
+              <hr>
+
+              <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-brown"></i>Habilidades</b></p>
+              
+              <p>Linguagem PHP</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 80%;">80%</div>
+              </div>
+
+              <p>Framework Laravel</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 70%;">70%</div>
+              </div>
+
+              <p>Linguagem Delphi</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 50%;">50%</div>
+              </div>
+
+              <p>Web(HTML 5 | CSS 3 | Javascript)</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 80%;">80%</div>
+              </div>
+
+              <p>Bootstrap 5</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 85%;">85%</div>
+              </div>
+
+              <p>MS-Office(Word | Excel | PowerPoint)</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 60%;">60%</div>
+              </div>
+
+              <p>Banco de Dados(SQL-Server | MySQL | Oracle)</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 85%;">85%</div>
+              </div>
+
+              <p>Containers Docker</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 70%;">70%</div>
+              </div>
+
+              <p>Versionamento Git e GitHub</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 80%;">80%</div>
+              </div>
+
+              <p>Sistemas Operacionais(Linux | Windows)</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-brown" style="width: 85%;">85%</div>
+              </div>
+              
+              <br>
+              <hr>
+
+              <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-brown"></i>Idiomas</b></p>
+              
+              <p>Português</p>
+              <div class="w3-light-grey w3-round-xlarge">
+                <div class="w3-round-xlarge w3-brown" style="height: 18.223px; width: 100%"></div>
+              </div>
+              
+              <p>Inglês</p>
+              <div class="w3-light-grey w3-round-xlarge">
+                <div class="w3-round-xlarge w3-brown" style="height: 18.223px; width: 30%"></div>
+              </div>
+
+              <p>Espanhol</p>
+              <div class="w3-light-grey w3-round-xlarge">
+                <div class="w3-round-xlarge w3-brown" style="height: 18.223px; width: 10%"></div>
+              </div>
+              
+              <br>
+
+            </div>
+          </div>
+          
+          <br>
+
+        <!-- End Left Column -->
+        </div>
+
+        <!-- Right Column -->
+        <div class="w3-twothird">
+
+          <div class="w3-container w3-card w3-white w3-margin-bottom w3-round">
+            
+            <h2 class="w3-text-grey w3-padding-16"><img class="w3-margin-right w3-xxlarge" src="images/icons/objective.png" style="width: 40px;"></i>Objetivo Profissional</h2>
+
+            <div class="w3-container">
+              <p>Utilizar minha sólida experiência em desenvolvimento web com PHP para criar soluções eficientes e inovadoras, contribuindo com minha equipe em projetos desafiadores e entregando resultados de alta qualidade.</p>
+            </div>
+
+          </div>        
+
+          <div class="w3-container w3-card w3-white w3-margin-bottom w3-round">
+            
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-brown"></i>Experiência Profissional</h2>
+            
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Web Programmer Analyst / Cantanhede Participações Holding</b></h5>
+              <h6 class="w3-text-brown"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Janeiro 2021 - Outubro 2023</h6>
+              
+              <p>
+                Codificava, testava e implementava funcionalidades e correções nos sites e nas aplicações.<br />
+                Levantava e entendia as necessidades dos clientes para criar soluções adequadas.<br />
+                Criava e gerenciava bancos de dados específicos junto a um DBA, garantindo eficiência e integridade dos dados.
+              </p>
+
+              <hr>
+            </div>
+
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Systems Programmer Assistant / Zoe Tecnologia – Software House</b></h5>
+              <h6 class="w3-text-brown"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Outubro 2018 - Outubro 2020</h6>
+              <p>
+                Auxiliava na codificação e implementação de novas funcionalidades nos sistemas e nas aplicações.<br />
+                Identificava e corrigia erros no código e realizava testes para garantir a qualidade do software.<br />
+                Registrava e atualizava a documentação técnica para facilitar a manutenção e compreensão do código.
+              </p>
+            </div>
+
+          </div>
+
+          <div class="w3-container w3-card w3-white w3-margin-bottom w3-round">
+            
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-brown"></i>Educação</h2>
+            
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>UNIP - Universidade Paulista</b></h5>
+              <h6 class="w3-text-brown"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2018 - 2021</h6>
+              <p>Tecnólogo em Análise e Desenvolvimento de Sistemas</p>
+              <hr>
+            </div>
+
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>E.E. Barão do Rio Branco</b></h5>
+              <h6 class="w3-text-brown"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2004 - 2017</h6>
+              <p>Ensino Fundamental é Médio</p>
+            </div>
+            
+          </div>
+
+          <div class="w3-container w3-card w3-white w3-margin-bottom w3-round">
+            
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-brown"></i>Cursos Adicionais</h2>
+            
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Impacta Tecnologia SP</b></h5>
+              <h6 class="w3-text-brown"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021/06 - 2021/12</h6>
+              <p>Web Developer Full Stack - (php, laravel e bootstrap) 120h</p>
+            </div>
+            
+          </div>
+
+          <div class="w3-container w3-card w3-white w3-round">
+            
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-users fa-fw w3-margin-right w3-xxlarge w3-text-brown"></i>Soft Skills</h2>
+            
+            <div class="w3-container">
+              <!--
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Comunicação Eficaz</p>              
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Inteligência Emocional</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Educado</p>
+              --> 
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Responsabilidade</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Proatividade</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Trabalho em equipe</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Adaptabilidade</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Atento aos detalhes</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Boa Comunicação</p>
+              <p><i class="fa fa-check-square fa-fw w3-margin-right w3-text-brown"></i>Esforçado e autodidata</p>
+            </div>
+          </div>          
+
+        <!-- End Right Column -->
+        </div>
+
+      <!-- End Grid -->
+      </div>
+
+      <!-- End Page Container -->
+    </div>
+
+    <footer class="w3-container w3-brown w3-center w3-margin-top w3-padding-16">
+      <p>Encontre-me nas redes sociais.</p>
+
+      <a href="https://www.linkedin.com/in/am-matheusoliveira/"        target="_blank">                  <i class="fa fa-linkedin  w3-hover-opacity fa-lg"></i></a>
+      <a href="https://github.com/am-matheusoliveira?tab=repositories" target="_blank" class="w3-margin"><i class="fa fa-github    w3-hover-opacity fa-lg"></i></a>
+      <a href="https://www.instagram.com/am.matheusoliveira/"          target="_blank">                  <i class="fa fa-instagram w3-hover-opacity fa-lg"></i></a>
+      
+      <!-- <p>Desenvolvido por <a href="https://www.linkedin.com/in/am-matheusoliveira/" target="_blank">Matheus Pereira</a></p> -->
+
+    </footer>
+
+  </body>
+</html>
